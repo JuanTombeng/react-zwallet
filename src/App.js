500b31ad0ea07459ea856f1ccb71e2b569e998e5
JuanTombeng <juantombeng03@gmail.com>
2022-01-10 02:45:55 +0700

    app js router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Pages/Login/index'
 import Signup from './Pages/Signup/index'
 import Home from './Pages/Home/index'
+import TransactionHistory from "./Pages/TransactionHistory";
+import Transfer from "./Pages/Transfer/Transfer";
+import TransferConfirmation from "./Pages/TransferConfirmation/TransferConfirmation";
+import Profile from "./Pages/Profile";
+import RequireAuth from "./Components/RequireAuth";
 
 const App = () => {
   return (
@@ -13,8 +18,11 @@ const App = () => {
         }/> */}
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
-        {/* <Route path="register" element={<SignUp />} /> */}
+        <Route path="/" element={<RequireAuth> <Home /> </RequireAuth>} />
+        <Route path="history" element={<RequireAuth><TransactionHistory /></RequireAuth>} />
+        <Route path="transfer" element={<RequireAuth><Transfer /></RequireAuth>} />
+        <Route path="/transfer/transfer-confirm" element={<RequireAuth><TransferConfirmation /></RequireAuth>} />
+        <Route path="profile" element={<RequireAuth><Profile /></RequireAuth>} />
       </Routes>
     </BrowserRouter>
   );
